0aa89817cd821809f07944787b16c204c4ff7593
Xiongyu Chen <chen_xiongyu@outlook.com>
2022-02-26 22:04:04 +0800

    The framework of the new paper has been done halfway, and the parameters need to be adjusted.

--- test.py
+++ test.py
@@ -1,54 +1,145 @@
+# Reservoir Computing and its Sensitivity to Symmetry in the Activation Function
+# 15 October 2020
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 100
-D = 3
 
-W_r = np.random.random((N, N))
-W_in = np.random.random((N, D))
+def lorenz_63(start_pos, time_tick, delta_t):
 
-Gamma = 10
-Sigma = 0.4
-Rou_in = 0.3
-Rou_r = 0.3
-K = 1
+    trajectory = np.zeros((len(start_pos), time_tick + 1))
+    trajectory[:, 0] = start_pos
 
-hyper_parameters = {"gamma": Gamma,
-                    "sigma": Sigma,
-                    "rou_in": Rou_in,
-                    "k": K,
-                    "rou_r": Rou_r}
+    for t in range(time_tick):
+        derivative = np.array([10 * (trajectory[1, t] - trajectory[0, t]),
+                               trajectory[0, t] * (28 - trajectory[2, t]) - trajectory[1, t],
+                               trajectory[0, t] * trajectory[1, t] - 8 / 3 * trajectory[2, t]])
+        trajectory[:, t + 1] = trajectory[:, t] + derivative * delta_t
+
+    return trajectory
 
 
-def w_in_construction(sigma, rou_in, n, d):
+def plot_trajectory(trajectory_1, trajectory_2=np.array([]), save_path=''):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot(trajectory_1[0, :], trajectory_1[1, :], trajectory_1[2, :])
 
-    w_in = np.random.randn(n, d) * rou_in
-    index = np.array(np.random.uniform(0, 1, (n, d)) < sigma, dtype=int)
+    if len(trajectory_2):
+        ax.plot(trajectory_2[0, :], trajectory_2[1, :], trajectory_2[2, :], '-')
 
-    return index * w_in
-
-
-def out_layer(w_out, f_out, r):
-
-    length = r.shape[1]
-    y = np.zeros(r.shape)
-
-    for t in range(length):
-        y[:, t] = np.dot(w_out, f_out(r[:, t]))
-
-    return y
+    if save_path:
+        plt.savefig(save_path)
+        plt.close()
 
 
-def forcasting(gamma, w_r, w_in, w_out, f_out, r_0, delta_t, forcast_time):
+def fix_nonzero_matrix_construction(k, n_1, n_2, sr=0.0, scale=0.0):
 
-    dimension = len(r_0)
-    r = np.zeros((dimension, forcast_time + 1))
-    r[:, 0] = r_0
+    matrix = np.random.uniform(-1, 1, (n_1, n_2))
+    for row in range(n_1):
+        index = np.random.choice(n_2, n_2 - k, replace=False)
+        matrix[row, :][index] = 0
 
-    for t in range(forcast_time):
+    if sr:
+        sr = sr / max(abs(np.linalg.eigvals(matrix)))
+        matrix = sr * matrix
 
-        derivative = -gamma * r[:, t] + gamma * np.tanh(np.dot(w_r, r[:, t]) +
-                                                        np.dot(w_in, np.dot(w_out, f_out(r[:, t]))))
+    if scale:
+        matrix = scale * matrix
+
+    return matrix
+
+
+def reservoir_derivative(w_r, w_in, r, trajectory):
+    return np.tanh(np.dot(w_r, r) + np.dot(w_in, trajectory))
+
+
+def reservoir_state(w_r, w_in, trajectory, r_0, delta_t):
+
+    length = trajectory.shape[1]
+
+    r = np.zeros((len(r_0), length))
+    r[:, 0] = r_0.T
+
+    for t in range(length - 1):
+        derivative = reservoir_derivative(w_r, w_in, r[:, t], trajectory[:, t])
         r[:, t + 1] = r[:, t] + derivative * delta_t
 
     return r
+
+
+def ridge_regression_matrix(reservoir, target, beta):
+
+    coef_matrix = np.dot(reservoir, reservoir.T)
+    coef_matrix = coef_matrix + beta * np.eye(len(coef_matrix))
+    target_matrix = np.dot(reservoir, target.T)
+
+    matrix = np.linalg.solve(coef_matrix, target_matrix)
+
+    return matrix.T
+
+
+def error_measures(trajectory_1, trajectory_2):
+
+    length = trajectory_2.shape[1]
+
+    index = np.array(np.linspace(0, length - 1, 50), dtype=int)
+    epsilon = np.sqrt(np.sum((trajectory_1[:, index] - trajectory_2[:, index]) ** 2) / 50)
+
+    return epsilon
+
+
+# Initial Setup
+N = 200
+D = 3
+
+A = 0.32
+Epsilon = 0.5
+S_input = A * (1 - Epsilon)
+Rou = A * Epsilon
+
+# Initial Matrix Setup
+W_in = fix_nonzero_matrix_construction(1, N, D, scale=S_input)
+W_r = fix_nonzero_matrix_construction(4, N, N, sr=Rou)
+
+# Initial Trajectory Setup
+Delta_t = 0.02
+Start_pos = np.array([20, 20, 20])
+
+T_train = 10500
+T_train = int(T_train / Delta_t)
+Trajectory = lorenz_63(Start_pos, T_train, Delta_t)
+
+# Initial Reservoir State Setup
+R_0 = np.zeros(N)
+Reservoir_state = reservoir_state(W_r, W_in, Trajectory, R_0, Delta_t)
+
+# Training
+Beta = 1.9e-11
+W_out = ridge_regression_matrix(Reservoir_state, Trajectory, Beta)
+Training = np.dot(W_out, Reservoir_state)
+plot_trajectory(Trajectory, Training)
+print(error_measures(Trajectory, Training))
+
+# Prediction
+T_test = 500
+T_test = int(T_test / Delta_t)
+
+Prediction_pos = Trajectory[:, -1]
+Prediction_trajectory = lorenz_63(Prediction_pos, T_test, Delta_t)
+
+Prediction_r_0 = Reservoir_state[:, -1]
+Prediction_reservoir_state = np.zeros((len(Prediction_r_0), T_test + 1))
+Prediction_reservoir_state[:, 0] = Prediction_r_0
+
+for Tick in range(T_test):
+
+    Prediction = np.dot(W_out, Prediction_reservoir_state[:, Tick])
+
+    Derivative = reservoir_derivative(W_r, W_in, Prediction_reservoir_state[:, Tick], Prediction)
+    Prediction_reservoir_state[:, Tick + 1] = Prediction_reservoir_state[:, Tick] + Derivative * Delta_t
+
+Prediction = np.dot(W_out, Prediction_reservoir_state)
+
+plot_trajectory(Prediction_trajectory, Prediction)
+
+print(error_measures(Prediction_trajectory, Prediction))
